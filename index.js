38f1c17da5ee5760eb9ce6f43945fe14179479a1
naveen <gameog8@gmail.com>
2026-02-28 13:39:09 +0530

    refactor:added nullish coalescing operator

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
     
  });
- console.log(process.env.SECRET_KEY);
+
  const openai = new OpenAIApi(configuration);
  app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(bodyParser.json({limit:"100mb" , extended:true}))
@@ -31,34 +31,9 @@ app.get("/",(req,res)=>{
 })
 
 
-//chat gpt
-app.post("/", async(req,res)=>{
-    try{
- const {message}= req.body;
-console.log(message);
-  const response =await openai.createCompletion({
-   model:"text-davinci-003",
-   prompt:`${message}`,
-   temperature:0,
-   max_tokens:3000,
-   top_p:1,
-   frequency_penalty:0.5,
-   presence_penalty:0,
-  
-  })
-  console.log(response);
-  console.log(response.data.choices[0].text)
-  res.status(200).send({
-    data:response.data.choices[0].text,
-  })
-    }catch(e){
-        console.log(e);
-        res.status(500).send({e});
-    }
-})
 
 const db = process.env.db
- const PORT= process.env.PORT ||3000 ;
+ const PORT= process.env.PORT ?? 5000 ;
 mongoose.connect(db).then(()=>{
     console.log("conn successfull")
 }).catch((e)=>{
